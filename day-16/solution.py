0dc0563e40ec48cba652e787eaac3dc92cab90c8
kac460 <kac460@rutgers.edu>
2022-12-27 14:41:26 -0500

    Day 16 part 1!

--- day-16/solution.py
+++ day-16/solution.py
@@ -46,7 +46,66 @@ def dist(s: str, e: str, graph: dict[str, list[str]]) -> int:
     return path_len
 
 
-def most_pressure_to_release(graph: dict[str, list[str]], rates: dict[str, int], s: str = 'AA', start_time: int = 30):
+def most_pressure_to_release_rec(
+    graph: dict[str, list[str]], 
+    rates: dict[str, int], 
+    s: str, 
+    t: int,
+    memo: dict[tuple[str, int, frozenset]] = None,
+    open_valves: frozenset[frozenset[str]] = frozenset()
+) -> int:
+    if memo is None:
+        memo = {}
+        possible_opens = powerset(
+            {
+                v for v in rates 
+                if (
+                    rates[v] > 0
+                    and
+                    dist(s, v, graph) < t
+                )
+            }
+        )
+        memo.update({(v, 0, p_o): 0 for v in graph for p_o in possible_opens})
+        memo.update({(v, 1, p_o): 0 for v in graph for p_o in possible_opens})
+    if (s, t, open_valves) in memo:
+        return memo[(s, t, open_valves)]
+    # memo[(s, start_time, frozenset({}))]
+    # best we can do if we skip opening s
+    skip = max(
+        most_pressure_to_release_rec(
+            graph=graph,
+            rates=rates, 
+            s=neighbor,
+            t=t-1,
+            memo=memo,
+            open_valves=open_valves
+        ) for neighbor in graph[s]
+    )
+    # if s is already open, obviously we skip oppening s s
+    if s in open_valves:
+        memo[(s, t, open_valves)] = skip
+        return skip
+    # Compute best if we open s
+    new_open_valves = open_valves.union({s})
+    release = rates[s] * (t-1) + (max(
+        most_pressure_to_release_rec(
+            graph=graph,
+            rates=rates,
+            s=neighbor,
+            t=t-2,
+            memo=memo,
+            open_valves=new_open_valves,
+        ) for neighbor in graph[s]
+    ) if len(new_open_valves) > 0 else 0)
+    if release > skip:
+        memo[s, t, open_valves] = release
+        return release
+    memo[s, t, open_valves] = skip
+    return skip
+
+
+def most_pressure_to_release(graph: dict[str, list[str]], rates: dict[str, int], s: str = 'AA', start_time: int = 30) -> int:
     # idea: make memo 3d
     # 3rd dimension would be frozenset of which valves are open
     # return memo[(s, 29?, empty_frozenset)]
@@ -56,41 +115,48 @@ def most_pressure_to_release(graph: dict[str, list[str]], rates: dict[str, int],
     #   release: rates[v] * (t-1) + max(val(c, t-2, open) for c in graph[v])
 
     # would never open valves with flow rate = 0
-    possible_opens = powerset({v for v in rates if rates[v] > 0})
+    possible_opens = powerset(
+        {
+            v for v in rates 
+            if (
+                rates[v] > 0
+                and
+                dist(s, v, graph) < start_time - 1
+            )
+        }
+    )
+    print(len(possible_opens))
     memo = {}
-    #memo = {(v, -1): 0 for v in graph}
     memo.update({(v, 0, p_o): 0 for v in graph for p_o in possible_opens})
     memo.update({(v, 1, p_o): 0 for v in graph for p_o in possible_opens})
     for t in range(2, start_time+1):
-        # Goal: set memo[(v, t)] for time 0 <= t <= start_time
-        #   (we don't need to check beyond start_time - dist)
-        # if t = 1: 
-        #   memo[(v, t)] = rates[v]
-        # Otherwise: 
-        #   (Either release v then move to best child or immediately move to best child)
-        #   (note in our input every v has non-empty neighbor list graph[v])
-        #   Release v: rates[v] + max(memo[(c, t-2)] for c in graph[v]) 
-        #   Move to best child: max(memo[(c, t-1) for c in graph[v]])
-        # apparently I misinterpreted the question :(
-
+        print(t)
         for v in graph:
-            for possible_open in possible_opens:
-                local_p_os = frozenset(
+            for open_valves in possible_opens:
+                # only need to check the child opens?
+                # (or something like that)
+                '''local_p_os = frozenset(
                     p_o for p_o in possible_opens 
                     # we know at least those in p_o must be open from here
-                    if p_o.issuperset(possible_open)
-                )
-                skip = max(memo[(c, t-1, p_o)] for c in graph[v] for p_o in local_p_os)
-                if v in possible_open:
-                    memo[(v, t, possible_open)] = skip
+                    if p_o.issuperset(open_valves)
+                )'''
+                #skip = max(memo[(c, t-1, p_o)] for c in graph[v] for p_o in local_p_os)
+                skip = max(memo[(c, t-1, open_valves)] for c in graph[v])
+                if rates[v] == 0 or v in open_valves:
+                    memo[(v, t, open_valves)] = skip
                 else: 
                     # if open v, eliminate those p_os where v not in p_o
-                    new_p_os = frozenset({p_o for p_o in local_p_os if v in p_o})
+                    '''new_p_os = frozenset({p_o for p_o in local_p_os if v in p_o})
                     release = rates[v] * (t-1) + (max(
                         memo[(c, t-2, p_o)] 
                         for c in graph[v] for p_o in new_p_os
-                    ) if len(new_p_os) > 0 else 0)
-                    memo[(v, t, possible_open)] = max(release, skip)
+                    ) if len(new_p_os) > 0 else 0)'''
+                    new_open_valves = open_valves.union({v})
+                    release = rates[v] * (t-1) + max(
+                        memo[(c, t-2, new_open_valves)]
+                        for c in graph[v]
+                    )
+                    memo[(v, t, open_valves)] = max(release, skip)
     return memo[(s, start_time, frozenset({}))]
 
 
@@ -106,4 +172,4 @@ def main(test: bool = False) -> None:
     
 
 if __name__ == '__main__':
-    main(test=True)
+    main(test=False)
